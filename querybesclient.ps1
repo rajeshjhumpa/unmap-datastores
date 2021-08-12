41e441b9fec27c61b1eed027c30b31d67f95f3b1
Prateek Siddhu <psiddhu@rms.com>
2021-08-13 02:11:18 +0530

    modified the querybesclient file

---
+++ querybesclient.ps1
@@ -0,0 +1,22 @@
+######################################################################################################
+# Script to quries "BesClient" service on all corporate virtual machines
+# Create Date: 10 June 2021
+# Original Authors : Prateek Siddhu
+# Updated By : Prateek Siddhu
+# Version : 1.0
+######################################################################################################
+
+$hosts   = $env:hosts.split()
+Write-Host "List of Servers: " $hosts
+
+$username	= $env:username
+$SecurePass = $env:password
+$password 	= ConvertTo-SecureString -String "$SecurePass" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+
+	foreach ($hosts in $hosts) {
+		Write-Host "Connecting to Server:" $hosts
+		Invoke-Command -ComputerName $hosts -Credential $credential -ScriptBlock {
+			Get-Service -Name "BesClient" | Select Status, Name, PSComputerName
+		}
+	}
